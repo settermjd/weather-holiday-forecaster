a64810ca76fe6a78a75e360dc5333081aa177dc2
Matthew Setter <matthew@matthewsetter.com>
2021-09-03 14:19:21 +0200

    Update the styling of the form to be that much more professional

--- public/index.php
+++ public/index.php
@@ -5,13 +5,17 @@
     <title>Weather Holiday Forecaster</title>
     <link rel="stylesheet" href="/css/styles.css">
 </head>
-<body class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 font-sans bg-gray-50">
+<body class="mt-4 mb-4 mx-auto max-w-7xl px-4 sm:mt-6 sm:mb-6 sm:px-6 md:mt-10 lg:mt-20 lg:mb-20 lg:px-8 xl:mt-28 xl:mb-28 font-sans bg-gray-50">
 <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl mb-6">Holiday Weather Forecaster</h1>
 <div class="text-base text-gray-500 mb-6">
-    Want to know if your holiday destination's going to be rained out, snowed in, or subject to a sandstorm? Register your details below and get daily weather updates for the 5 days prior to your trip up until the day that you head home.
+    <span>Want to know if your holiday destination's going to be rained out, snowed in, or subject to a sandstorm?</span>
+    <span class="block mt-4 sm:inline sm:mt-0">Register your details below and get daily weather updates for the 5 days prior to your trip up until the day that
+        you head home.</span>
+</
+>
 </div>
 <form>
-    <div class="border-2 rounded-md border-gray-200 bg-white p-2 md:p-6 mb-6">
+    <div class="border-2 rounded-md border-gray-200 bg-white p-4 sm:p-6 md:p-8 mb-6">
         <div class="">
             <div class="mb-4">
                 <input class="border-2 rounded-md border-gray-200 w-full p-4 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50"
@@ -59,7 +63,11 @@
             <button tabindex="6" class="w-full md:w-auto flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50"><span>Register</span></button>
         </div>
     </div>
-    <script src="js/app.js"
+    <div class="pl-2 text-center">
+        <span class="block text-sm text-gray-400">copyright &copy; 2021 Matthew Setter. All rights reserved.</span>
+        <span class="text-sm text-gray-400">For support, email <a href="mailto:msetter@twilio.com" class="text-blue-500 underline">msetter@twilio.com</a>.</span>
+    </div>
+    <script src="js/app.js">
 </form>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
+    './public/**/*.html',
+    './public/**/*.php',
     './src/**/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
